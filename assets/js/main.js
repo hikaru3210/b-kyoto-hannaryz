a12276b96894b8659374693d1ed363df147ded1c
hiroyuzu <89890218+hiroyuzu@users.noreply.github.com>
2021-09-19 04:59:22 +0900

    update answer data of stage2

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,8 +42,9 @@ const app = Vue.createApp({
         ],
         stage2: [
           false, // 2-1
-          // false, // 2-2
-          // false, // 2-3
+          false, // 2-2
+          false, // 2-3
+          false, // 2-4
         ],
         stage3: [
           false, // 3-1
